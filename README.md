14d124a31bc6ca749880081dc968d236bea7f178
Simon Oulevay <simon.oulevay@gmail.com>
2017-11-21 11:18:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,215 @@
+# express-jwt-policies
+
+Lightweight [JWT][jwt] authentication & authorization middleware for [Express][express].
+
+[![npm version](https://badge.fury.io/js/express-jwt-policies.svg)](https://badge.fury.io/js/express-jwt-policies)
+[![License](https://img.shields.io/badge/License-MIT-blue.svg)](LICENSE.txt)
+
+<!-- START doctoc generated TOC please keep comment here to allow auto update -->
+<!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->
+
+
+- [Usage](#usage)
+  - [Authentication only](#authentication-only)
+  - [Authorization only](#authorization-only)
+  - [Asynchronous authorization](#asynchronous-authorization)
+- [Configuration](#configuration)
+  - [Module options](#module-options)
+  - [Authentication options](#authentication-options)
+  - [Authorization options](#authorization-options)
+
+<!-- END doctoc generated TOC please keep comment here to allow auto update -->
+
+
+
+## Usage
+
+**jwt-express-policies** is an opinionated JWT-based authentication &
+authorization middleware.  It assumes that:
+
+* Authentication is performed by sending a JWT bearer token in the
+  Authorization header.
+* Some JWTs may optionally correspond to a resource (e.g. a user in the
+  database) that you will need to load.
+
+This module does **not** handle authentication or authorization errors for you.
+It simply passes them down the middleware chain, leaving you the responsibility
+of responding adequately to the user:
+
+* Authentication errors will have the `status` property set to [401][http-401].
+  Properties may also be added by [express-jwt][express-jwt] which is used to
+  check the JWT.
+* Authorization errors will have the `status` property set to [403][http-403].
+
+```js
+const express = require('express');
+const jwtExpressPolicies = require('jwt-express-policies');
+
+// Configure the module.
+const authorize = jwtExpressPolicies({
+
+  // Provide a function to load the authenticated resource.
+  authenticatedResourceLoader: function(req, res, next) {
+    new User().where({ id: req.jwtToken.sub }).then(user => {
+      req.currentUser = user;
+      next();
+    }).catch(next);
+  },
+
+  // Provide the secret used to sign JWTs.
+  jwtSecret: 'changeme'
+});
+
+// Create policies.
+const stuffPolicy = {
+
+  // Any user can retrieve stuff.
+  canRetrieve: function(req) {
+    return req.currentUser;
+  },
+
+  // Only admins can create stuff.
+  canCreate: function(req) {
+    return req.currentUser && req.currentUser.hasRole('admin');
+  }
+};
+
+// Create your routes and plug in authorization as middleware.
+const router = express.Router();
+
+router.get('/protected/stuff',
+  authorize(stuffPolicy.canRetrieve),
+  function(req, res, next) { /* retrieve implementation */ });
+
+router.post('/protected/stuff',
+  authorize(stuffPolicy.canCreate),
+  function(req, res, next) { /* create implementation */ });
+```
+
+### Authentication only
+
+```js
+// Configure the module.
+const auth = jwtExpressPolicies({
+  // ...
+});
+
+router.get('/protected/stuff',
+  auth.authenticate(),
+  function(req, res, next) { /* retrieve implementation */ });
+```
+
+### Authorization only
+
+```js
+// Configure the module.
+const auth = jwtExpressPolicies({
+  // ...
+});
+
+// Use the `authenticate` option of the `authorize` function to
+router.get('/protected/stuff',
+  auth.authorize(policy.canRetrieve, { authenticate: false }),
+  function(req, res, next) { /* retrieve implementation */ });
+```
+
+### Asynchronous authorization
+
+Policy functions may return a promise to perform asynchronous checks:
+
+```js
+const stuffPolicy = {
+  canCreate: async function(req) {
+    if (!req.currentUser) {
+      return false;
+    } else {
+      const permissions = await fetchUserPermissions(req.currentUser);
+      return permissions.indexOf('stuff:create') >= 0;
+    }
+  }
+};
+```
+
+
+
+## Configuration
+
+### Module options
+
+These options can be passed to the function returned by
+`require('jwt-express-policies')` to configure the module:
+
+* `jwtSecret` (string) **required** - The secret used to sign JWTs.
+
+* `jwtRequestProperty` (string) - The property of the request to which the JWT
+  should be attached (`jwtToken` by default).
+
+  ```js
+  const auth = jwtExpressPolicies({
+    jwtSecret: 'changeme',
+    jwtRequestProperty: 'token',
+    // ...
+  });
+  ```
+
+* `authenticatedResourceLoader` (function) **required** - An Express middleware
+  function that will be called when a valid JWT bearer token is sent in the
+  Authorization header. The JWT will be available as the `req.jwtToken`
+  property (by default). It should load whatever resource is identified by the
+  token (e.g. a user) and attach it to the request (e.g. to the
+  `req.currentUser` property) if necessary. You may do nothing but call
+  `next()` in this function if the JWT is sufficient and nothing needs to be
+  loaded.
+
+  ```js
+  const auth = jwtExpressPolicies({
+    authenticatedResourceLoader: function(req, res, next) {
+      new User().where({ id: req.jwtToken.sub }).then(user => {
+        req.currentUser = user;
+        next();
+      }).catch(next);
+    },
+    // ...
+  });
+  ```
+
+* `authenticationRequired` (boolean) - If true, successful authentication is
+  always required. This option is not set by default. Successful authentication
+  is required by default when calling the `authenticate` function alone, but
+  not the `authorize` function (see [Authorization options][authorization-options]).
+
+### Authentication options
+
+These options can be passed when calling the module's `authenticate` function:
+
+* `authenticationRequired` (boolean) - Whether successful authentication is
+  required. If true, an error will be passed through the middleware chain if no
+  valid JWT is found in the Authorization header. If false, an unauthenticated
+  request will not cause an error, but the authenticated resource loader will
+  not be called. This defaults to the value of the `authenticationRequired`
+  option provided when configuring the module, or to true if there was none.
+
+The entire options object (including any custom option you might add) is
+attached to the request as `req.authOptions`.
+
+### Authorization options
+
+These options can be passed when calling the `authorize` function (which is
+also the function returned by configuring the module):
+
+* `authenticate` (boolean) - Whether to perform authentication before
+  authorization. Defaults to true.
+* `authenticationRequired` (boolean) - Whether successful authentication is
+  required before performing authorization with the policy function. Defaults
+  to false (unauthenticated requests **will** call the policy function).
+
+The entire options object (including any custom option you might add) is
+attached to the request as `req.authOptions`.
+
+
+
+[express]: https://expressjs.com
+[express-jwt]: https://github.com/auth0/express-jwt
+[http-401]: https://httpstatuses.com/401
+[http-403]: https://httpstatuses.com/403
+[jwt]: https://jwt.io
---
+++ index.js
@@ -0,0 +1,220 @@
+const compose = require('composable-middleware');
+const jwt = require('express-jwt');
+
+// Returns a function that can be used as an authentication & authorization middleware.
+//
+// Options:
+//
+// * `jwtSecret` (string) **required** - The secret used to sign JWTs.
+//
+// * `jwtRequestProperty` (string) - The property of the request to which the JWT should be
+//   attached ("jwtToken" by default).
+//
+// * `authenticatedResourceLoader` (function) **required** - An Express middleware function that
+//   will be called when a valid JWT bearer token is sent in the Authorization header. The JWT will
+//   be available as the `req.jwtToken` property (by default). It should load whatever resource is
+//   identified by the token (e.g. a user) and attach it to the request (e.g. to the `req.currentUser`
+//   property). You may do nothing but call `next()` in this function if the JWT is sufficient and
+//   nothing needs to be loaded.
+//
+// * `authenticationErrorHandler` (function) - An optional Express error-handling middleware function
+//   that will be called if an authentication error occurs. This may be due to an invalid JWT, or to
+//   a missing JWT if `authenticationRequired` is true. You may handle or transform the error to fit
+//   your application.
+//
+// * `authenticationRequired` (boolean) - If true, successful authentication is always required.
+//   This option is not set by default. Successful authentication is required by default when calling
+//   the `authenticate` function, but not the `authorize` function (see their respective documentation).
+//
+//     const express = require('express');
+//     const jwtExpressPolicies = require('jwt-express-policies');
+//
+//     const auth = jwtExpressPolicies({
+//
+//       authenticatedResourceLoader: function(req, res, next) {
+//         new User().where({ id: req.jwtToken.sub }).then(user => {
+//           req.currentUser = user;
+//           next();
+//         }).catch(next);
+//       },
+//
+//       jwtSecret: 'changeme'
+//     });
+//
+//     const stuffPolicy = {
+//       canRetrieve: function(req) {
+//         return req.currentUser;
+//       },
+//
+//       canCreate: function(req) {
+//         return req.currentUser && req.currentUser.hasRole('admin');
+//       }
+//     };
+//
+//     const router = express.Router();
+//
+//     router.get('/protected/stuff',
+//       auth.authorize(stuffPolicy.canRetrieve),
+//       function(req, res, next) { /* retrieve implementation */ });
+//
+//     router.post('/protected/stuff',
+//       auth.authorize(stuffPolicy.canCreate),
+//       function(req, res, next) { /* create implementation */ });
+module.exports = function(options) {
+  if (!options) {
+    throw new Error('Middleware options are required');
+  }
+
+  const authenticatedResourceLoader = options.authenticatedResourceLoader;
+  if (typeof(authenticatedResourceLoader) != 'function') {
+    throw new Error('The "authenticatedResourceLoader" option must be an Express middleware function');
+  }
+
+  const authenticationErrorHandler = options.authenticationErrorHandler;
+  const authenticationRequired = options.authenticationRequired !== undefined ? !!options.authenticationRequired : undefined;
+
+  const jwtSecret = options.jwtSecret;
+  if (!jwtSecret) {
+    throw new Error('The "jwtSecret" option is required');
+  }
+
+  const jwtRequestProperty = options.jwtRequestProperty || 'jwtToken';
+
+  const authorizationErrorHandler = options.authorizationErrorHandler;
+
+  const middleware = authorize;
+  middleware.authenticate = authenticate;
+  middleware.authorize = authorize;
+
+  return middleware;
+
+  // Authenticates a resource (e.g. a user) through a JWT Bearer token in the Authorization header.
+  // This calls the authenticated resource loader provided when configuring the module, which may
+  // attach properties to the request, e.g. `req.currentUser`.
+  //
+  // Options:
+  //
+  // * `authenticationRequired` (boolean) - Whether authentication is required. If true, an error will
+  //   be passed through the middleware chain if no valid JWT is found in the Authorization header. If
+  //   false, an unauthenticated request will not cause an error, but the authenticated resource loader
+  //   will not be called. This defaults to the value of the `authenticationRequired` option provided
+  //   when configuring this module, or to true if there was none.
+  //
+  // The entire options object (including any custom option you might add) is attached to the request
+  // as `req.authOptions`.
+  function authenticate(options) {
+    if (!options) {
+      options = {};
+    }
+
+    // Require a JWT by default, unless specified otherwise in the function's or module's options.
+    let jwtRequired = true;
+    if (options.authenticationRequired !== undefined) {
+      jwtRequired = !!options.authenticationRequired;
+    } else if (authenticationRequired !== undefined) {
+      jwtRequired = authenticationRequired;
+    }
+
+    let chain = compose()
+      // Parse the JWT bearer token, if any.
+      .use(validateJwt(jwtRequired))
+      // Add the 401 status to the JWT error, if any.
+      .use(enrichJwtError)
+      // Run the provided function to load the authenticated resource.
+      .use((req, res, next) => {
+        if (req.jwtToken) {
+          authenticatedResourceLoader(enrichRequest(req, options), res, next);
+        } else {
+          next();
+        }
+      });
+
+    // Plug in the provided authentication error handler, if any.
+    if (authenticationErrorHandler) {
+      chain = chain.use((err, req, res, next) => authenticationErrorHandler(err, enrichRequest(req, options), res, next));
+    }
+
+    return chain;
+  }
+
+  // Ensures that the currently authenticated resource (e.g. a user) is authorized to perform the request by
+  // using a policy function. The policy function is called with the request object as its first argument, and
+  // has the responsibility to check that everything is in order and return true if the request should be
+  // authorized, or false otherwise. Authentication is performed first (by default), so the provided
+  // authenticated resource loader will have run and may have attached a resource to the request (e.g.
+  // `req.currentUser`).
+  //
+  // By default, authentication is optional when called during authorization, meaning that an unauthenticated
+  // request will pass through to the authorization step, and that it is the responsibility of the policy
+  // function to ensure that the request is actually authenticated (e.g. by checking `req.currentUser`). To
+  // change this behavior, set the `authenticationRequired` option in this function or when configuring the
+  // module.
+  //
+  // Options:
+  //
+  // * `authenticate` (boolean) - Whether to perform authentication before authorization. Defaults to true.
+  // * `authenticationRequired` (boolean) - Whether successful authentication is required before performing
+  //   authorization with the policy function. Defaults to false.
+  //
+  // The entire options object (including any custom option you might add) is attached to the request
+  // as `req.authOptions`. It is also passed to the authentication function.
+  function authorize(policy, options) {
+    if (typeof(policy) !== 'function') {
+      throw new Error('Policy must be a function');
+    } else if (!options) {
+      options = {};
+    }
+
+    const performAuthentication = options.authenticate === undefined || options.authenticate;
+    if (options.authenticationRequired === undefined) {
+      options.authenticationRequired = false;
+    }
+
+    let chain = compose();
+
+    // Perform authentication (if enabled).
+    if (performAuthentication) {
+      chain = chain.use(authenticate(options));
+    }
+
+    // Perform authorization by calling the policy function.
+    chain = chain.use(function(req, res, next) {
+      Promise.resolve().then(() => policy(enrichRequest(req, options))).then(authorized => {
+        if (!authorized) {
+          throw authError(403, 'You are not authorized to access this resource.');
+        }
+      }).then(next, next);
+    });
+
+    // Plug in in the provided authorization error handler, if any.
+    if (authorizationErrorHandler) {
+      chain = chain.use((err, req, res, next) => authorizationErrorHandler(err, enrichRequest(req, options), res, next));
+    }
+
+    return chain;
+  }
+
+  function validateJwt(required) {
+    return jwt({
+      credentialsRequired: required,
+      requestProperty: jwtRequestProperty,
+      secret: jwtSecret
+    });
+  }
+
+  function enrichJwtError(err, req, res, next) {
+    err.status = 401;
+    next(err);
+  }
+
+  function enrichRequest(req, options) {
+    req.authOptions = options;
+    return req;
+  }
+
+  function authError(status, message) {
+    const authError = new Error(message);
+    authError.status = status;
+    throw authError;
+  }
+}
